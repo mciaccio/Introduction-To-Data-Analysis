00693e9834190f18326f89173a64bed144147a4b
Menfi Systems <Menfi@Michaels-Mac-mini.local>
2017-01-15 04:19:10 -0600

    Examples adding a Pandas Series to Pandas DataFrame

---
+++ addingADataFrameToASeries14of21.py
@@ -0,0 +1,148 @@
+'''
+Created on Jan 15, 2017
+
+@author: Menfi
+'''
+
+import pandas as pd 
+
+print("\nBegin\n")
+
+# Change False to True for each block of code to see what it does
+
+# Example - Adding a Series to a square DataFrame
+# integers of the Pandas Series are added to the integers of the Pandas DataFrame 
+if True:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({
+        0: [10, 20, 30, 40],
+        1: [50, 60, 70, 80],
+        2: [90, 100, 110, 120],
+        3: [130, 140, 150, 160]
+    })
+    
+    # print df
+    # print '' # Create a blank line between outputs
+    # print df + s
+    
+    # print("df -> ")
+    print(df)
+    # print("type(df) - {}".format(type(df)))
+    #        type(df) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+    
+    # print("df + s -> ")
+    print(df + s)
+    # print("type(df + s) - {}".format(type(df + s)))
+    #        type(df + s) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+    
+# Adding a Series to a one-row DataFrame 
+# integers of the Pandas Series are added to the integers of the Pandas DataFrame 
+# Example - create, instantiate, populate 1 row Pandas DataFrame  
+if False:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({0: [10], 1: [20], 2: [30], 3: [40]})
+    
+    # print df
+    # print '' # Create a blank line between outputs
+    # print df + s
+    
+    # print("df -> ")
+    print(df)
+    # print("type(df) - {}".format(type(df)))
+    #        type(df) - <class 'pandas.core.frame.DataFrame'>
+
+    # print("")
+    
+    # print("df + s -> ")
+    print(df + s)
+    # print("type(df + s) - {}".format(type(df + s)))
+    #        type(df + s) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+
+# Adding a Series to a one-column DataFrame
+# Pandas Series Integers added to the first column, NaN all the rest 
+# Example - create, instantiate, populate 1 column Pandas DataFrame  
+if False:
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({0: [10, 20, 30, 40]})
+    
+    # print df
+    # print '' # Create a blank line between outputs
+    # print df + s
+    
+    # print("df -> ")
+    print(df)
+    # print("type(df) - {}".format(type(df)))
+    # type(df + s) - <class 'pandas.core.frame.DataFrame'>
+
+    # print("")
+    
+    # print("80df + s -> ")
+    print(df + s)
+    # print("type(df + s) - {}".format(type(df + s)))
+    # type(df + s) - <class 'pandas.core.frame.DataFrame'>
+    # print("")
+    
+# Adding when DataFrame column names match Series index
+# Pandas Series integers successfully added to Pandas DataFrame integers 
+if False  :
+    s = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+    df = pd.DataFrame({
+        'a': [10, 20, 30, 40],
+        'b': [50, 60, 70, 80],
+        'c': [90, 100, 110, 120],
+        'd': [130, 140, 150, 160]
+    })
+    
+    # print df
+    # print '' # Create a blank line between outputs
+    # print df + s
+    
+    # print("df -> ")
+    print(df)
+    # print("type(df) - {}".format(type(df)))
+    # type(df) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+    
+    # print("df + s -> ")
+    print(df + s)
+    # print("type(df + s) - {}".format(type(df + s)))
+    # type(df + s) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+    
+# Adding when DataFrame column names don't match Series index
+# Result large Pandas DataFrame all Nan 
+if False :
+    s = pd.Series([1, 2, 3, 4])
+    df = pd.DataFrame({
+        'a': [10, 20, 30, 40],
+        'b': [50, 60, 70, 80],
+        'c': [90, 100, 110, 120],
+        'd': [130, 140, 150, 160]
+    })
+    
+    # print df
+    # print '' # Create a blank line between outputs
+    # print df + s
+
+    print("df -> ")
+    print(df)
+    # print("type(df) - {}".format(type(df)))
+    # type(df) - <class 'pandas.core.frame.DataFrame'>
+    
+    print("")
+    
+    print("df + s -> ")
+    print(df + s)
+    # print("type(df + s) - {}".format(type(df + s)))
+    # type(df + s) - <class 'pandas.core.frame.DataFrame'>
+
+    print("")
+
